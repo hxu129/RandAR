92d28931a12bc9f9a46db083bdc407bf262cef51
Hua XU <hxu401@connect.hkust-gz.edu.cn>
2025-07-24 11:12:37 +0000

    Modified scripts

--- slurm_scripts/eval_corrector.sh
+++ slurm_scripts/eval_corrector.sh
@@ -10,15 +10,21 @@ else
     MODEL_SIZE_LATTER=xl
 fi
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nproc_per_node=4 $PROJECT_DIR/sample_corrector.py \
+CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nproc_per_node=1 $PROJECT_DIR/tools/search_cfg_weights_corrector.py \
+    --config "${PROJECT_DIR}/configs/randar/randar_${MODEL_SIZE_LATTER}_${MODEL_SIZE}_${TOKENIZER}.yaml" \
     --exp-name $MODEL_NAME-test-pipeline \
     --gpt-ckpt "${DATA_PATH}/pretrain-weights/randar_${MODEL_SIZE}_${TOKENIZER}_360k_bs_1024_lr_0.0004.safetensors" \
     --vq-ckpt $PROJECT_DIR/tokenizer/${TOKENIZER}/vq_ds16_c2i.pt \
-    --config "${PROJECT_DIR}/configs/randar/randar_${MODEL_SIZE_LATTER}_${MODEL_SIZE}_${TOKENIZER}.yaml" \
+    --per-proc-batch-size 100 \
+    --num-fid-samples-search 100 \
+    --num-fid-samples-final 50000 \
+    --cfg-scales-interval 0.2 \
+    --cfg-scales-search 2.0,8.0 \
+    --results-path $DATA_PATH/results \
+    --ref-path $DATA_PATH/VIRTUAL_imagenet256_labeled.npz \
+    --sample-dir $DATA_PATH/evaluation \
     --corrector-config "${PROJECT_DIR}/configs/corrector/corrector.yaml" \
     --corrector-ckpt "${DATA_PATH}/corrector-results/test-pipeline_bs_1536_lr_0.0001/checkpoints/iters_00001000/model.safetensors" \
-    --cfg-scales 1.0,4.0 \
-    --sample-dir $DATA_PATH/samples \
-    --num-inference-steps 88 \
     --corrector-threshold 0.5 \
-    --corrector-max-steps 5
+    --corrector-max-steps 5 \
+    --num-inference-steps 88
