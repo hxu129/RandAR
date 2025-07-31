ccfa8075714d64b84def84254090c92606f0965f
Hua XU <hxu401@connect.hkust-gz.edu.cn>
2025-07-31 07:23:00 +0000

    Correct eval file reading problem

--- tools/search_cfg_weights_corrector.py
+++ tools/search_cfg_weights_corrector.py
@@ -32,12 +32,15 @@ def create_npz_from_sample_folder(sample_dir, num=50_000):
     Builds a single .npz file from a folder of .png samples.
     """
     samples = []
-    for i in tqdm(range(num), desc="Building .npz file from samples"):
-        sample_pil = Image.open(f"{sample_dir}/{i:06d}.png")
-        sample_np = np.asarray(sample_pil).astype(np.uint8)
-        samples.append(sample_np)
+    # change read all the png files in the sample_dir
+    # for i in tqdm(range(num), desc="Building .npz file from samples"):
+    for i in tqdm(os.listdir(sample_dir), desc="Building .npz file from samples"):
+        if i.endswith(".png"):
+            sample_pil = Image.open(f"{sample_dir}/{i}")
+            sample_np = np.asarray(sample_pil).astype(np.uint8)
+            samples.append(sample_np)
     samples = np.stack(samples)
-    assert samples.shape == (num, samples.shape[1], samples.shape[2], 3)
+    # assert samples.shape == (num, samples.shape[1], samples.shape[2], 3)
     npz_path = f"{sample_dir}.npz"
     np.savez(npz_path, arr_0=samples)
     print(f"Saved .npz file to {npz_path} [shape={samples.shape}].")
